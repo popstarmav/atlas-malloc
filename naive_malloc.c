d710f9fc8da74c0f34e86004d94bdd0a89587542
popstarmav <cj.johnson2017@gmail.com>
2025-01-23 12:04:28 -0600

    malloc

--- naive_malloc.c
+++ naive_malloc.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 #include "malloc.h"
 
-static void *page_start = NULL
-static size_t page_size = 0
-static size_t used_space = 0
+static void *page_start;
+static size_t page_size;
+static size_t used_space;
 
 /**
  * naive_malloc - Allocates memory of requested size
@@ -17,28 +17,19 @@ void *naive_malloc(size_t size)
 	size_t *header;
 	size_t aligned_size;
 
-	/* Initialize page size */
 	if (page_size == 0)
-	{
 		page_size = sysconf(_SC_PAGESIZE);
-	}
 
-	/* Align the size to 8 bytes (64-bit alignment) */
 	aligned_size = (size + sizeof(size_t) + 7) & ~7;
 
-	/* First allocation or need new page */
 	if (page_start == NULL || (used_space + aligned_size > page_size))
 	{
 		page_start = sbrk(page_size);
 		if (page_start == (void *)-1)
-		{
 			return (NULL);
-		}
-
 		used_space = 0;
 	}
 
-	/* Set up header and allocation */
 	header = (size_t *)((char *)page_start + used_space);
 	*header = size;
 	used_space += aligned_size;
