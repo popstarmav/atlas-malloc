51b254074d02bf30aea6148d04f6aa09451b4f6f
popstarmav <cj.johnson2017@gmail.com>
2025-01-27 09:53:15 -0600

    malloc

--- malloc.c
+++ malloc.c
@@ -2,9 +2,6 @@
 #include <stddef.h>
 #include "malloc.h"
 
-static void *heap_start;
-static size_t total_size;
-
 /**
  * _malloc - Allocates memory on the heap
  * @size: Number of bytes to allocate
@@ -13,25 +10,22 @@ static size_t total_size;
  */
 void *_malloc(size_t size)
 {
+	void *block;
 	size_t *header;
-	size_t aligned_size;
-	size_t page_size;
+	size_t total_size;
 
-	page_size = sysconf(_SC_PAGESIZE);
-	aligned_size = (size + sizeof(size_t) + 7) & ~7;
+	if (size == 0)
+		return (NULL);
 
-	if (heap_start == NULL || (total_size + aligned_size > page_size))
-	{
-		heap_start = sbrk(page_size);
-		if (heap_start == (void *)-1)
-			return (NULL);
-		total_size = 0;
-	}
+	total_size = size + sizeof(size_t);
+	block = sbrk(total_size);
 
-	header = (size_t *)((char *)heap_start + total_size);
+	if (block == (void *)-1)
+		return (NULL);
+
+	header = (size_t *)block;
 	*header = size;
-	total_size += aligned_size;
 
-	return ((void *)(header + 1));
+	return (header + 1);
 }
 
